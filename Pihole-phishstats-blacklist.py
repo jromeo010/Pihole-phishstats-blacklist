1e0125e86c157bb640361104d717bf29385d2954
jrome010 <jrome010@odu.edu>
2021-05-07 16:43:19 -0400

    requests testing

--- Pihole-phishstats-blacklist.py
+++ Pihole-phishstats-blacklist.py
@@ -1,1 +1,8 @@
+import requests
 print('hello world')
+
+
+url = 'https://phishstats.info:2096/api/phishing?_where=(date,gt,1900-01-06T12:03:00.000Z)&_p=1&_size=100'
+
+test = requests.get(url)
+print(test.json())
