4d29a0d487c99586464ceb675616b4c27542d420
jrome010 <jrome010@odu.edu>
2021-05-10 20:50:01 -0400

    Started building looping for script

--- Pihole-phishstats-blacklist.py
+++ Pihole-phishstats-blacklist.py
@@ -1,8 +1,29 @@
-import requests
-print('hello world')
+import requests, time
 
 
-url = 'https://phishstats.info:2096/api/phishing?_where=(date,gt,1900-01-06T12:03:00.000Z)&_p=1&_size=100'
+def main():
 
-test = requests.get(url)
-print(test.json())
+    TS = '2021-05-10T12:00:00.000Z'
+    Loop_control = True
+    page_tracker = 1
+
+
+    while Loop_control:
+        url = 'https://phishstats.info:2096/api/phishing?_where=(date,gt,'+TS+')&_p='+str(page_tracker)+'&_size=100'
+        RESPONSE = requests.get(url)
+        JSON_PAYLOAD = []
+        JSON_PAYLOAD = RESPONSE.json()
+        page_tracker += 1
+
+        for dictionary in JSON_PAYLOAD:
+            print(dictionary['url'])
+
+
+        if not len(JSON_PAYLOAD)== 100:
+            Loop_control = False
+            break
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
