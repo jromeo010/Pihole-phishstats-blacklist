eefac53195f8a4b64837c21021848801eee23775
jromeo010 <54372021+jromeo010@users.noreply.github.com>
2021-06-02 21:46:58 -0400

    Create Pihole-phishstats-blacklist.py

--- Pihole-phishstats-blacklist.py
+++ Pihole-phishstats-blacklist.py
@@ -1,5 +1,23 @@
-import requests, time, re
+import requests,time,re,os
 
+def delta_file():
+    DELTA_FILE = 'phishstats_delta.txt'
+    TS = ''
+    TODAY = time.strftime('%Y-%m-%dT%H:%M:%S.000Z',time.localtime())
+    if os.path.exists(DELTA_FILE):
+        FILE_ = open(DELTA_FILE, 'r+')
+        TS = FILE_.readlines()
+        FILE_.truncate(0)
+        FILE_.write(TODAY)
+        FILE_.close()
+        print('Updating Delta File with '+str(TODAY))
+    else:
+        print("Delta File did not exists, creating file: "+DELTA_FILE)
+        FILE_ = open(DELTA_FILE, 'w+')
+        TS = TODAY
+        FILE_.write(TODAY)
+        FILE_.close()
+    return str(TS[0])
 
 def call_api(TS,PAGE_TRACKER):
         url = 'https://phishstats.info:2096/api/phishing?_size=100&_where=(date,gt,'+TS+')&_p='+str(PAGE_TRACKER)
@@ -8,14 +26,9 @@ def call_api(TS,PAGE_TRACKER):
         JSON_PAYLOAD = RESPONSE.json()
         return JSON_PAYLOAD
 
-
-def main():
-
-    TS = '2021-06-02T20:40:21.000Z'
+def Fetch_Results(TS):
     Loop_control = True
     page_tracker = 1
-
-    TODAY = time.strftime('%Y-%m-%dT%H:%M:%S.000Z',time.localtime())
     DOMAIN_LIST=[]
 
     while Loop_control:
@@ -26,8 +39,6 @@ def main():
             Loop_control = False
             print('error in api call, quiting script')
             break
-        page_tracker += 1
-
 
         for OBJ in JSON_PAYLOAD:
             DOMAIN_EXTRACT = re.findall(r'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]',OBJ['url'])#extracts domain from url
@@ -36,10 +47,20 @@ def main():
         if not len(JSON_PAYLOAD)== 100:
             Loop_control = False
             break
+
         time.sleep(5)
+        page_tracker += 1
 
-    DOMAIN_LIST = list(set(DOMAIN_LIST))#remove duplicates from list
+    return DOMAIN_LIST
 
+def main():
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    TS = delta_file()
+    DOMAIN_LIST = list(set(Fetch_Results(TS)))#remove duplicates from list
+    print(DOMAIN_LIST)
 
 if __name__ == '__main__':
     main()
